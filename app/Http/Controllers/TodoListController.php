31fe47d11c1774440cbebf4472191a59cc79ac2b
Bashir Abdul <freakybii@gmail.com>
2023-09-24 22:50:46 +0100

    push to git

---
+++ app/Http/Controllers/TodoListController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\ListItem;
+
+class TodoListController extends Controller
+{
+    //
+    public function index() {
+        return view('welcome', ['listItems' => ListItem::where('is_complete', 0)->get()]);
+    }
+
+    public function markComplete($id) {
+        // \Log::info($id);
+        $listItem = ListItem::find($id);
+        // \Log::info($listItem);
+        $listItem->is_complete = 1;
+        $listItem->save();
+
+        return redirect('/');
+    }
+
+    public function saveItem(Request $request) {
+        // \Log::info(json_encode($request->all()));
+
+        $newListItem = new ListItem;
+        $newListItem->name = $request->listItem;
+        $newListItem->is_Complete = 0;
+        $newListItem->save();
+        
+        // return view('welcome', ['listItems' => ListItem::all()]);
+        return redirect('/');
+    }
+}
